824f1b968d32e0d6e2530420a3d3f5a2e4d399cb
Nguyen Thanh Bao <31978177+Paulvn2017@users.noreply.github.com>
2018-03-22 23:04:17 +0700

    Delete create_git_.bat

--- create_git_.bat
+++
@@ -1,9 +0,0 @@
-git init
-git remote add origin https://github.com/baodev01/TaskList.git
-git pull
-git branch branch_20180322
-git checkout branch_20180322
-git branch --set-upstream-to=origin/branch_20180322
-git pull
-#git ls-files --others --exclude-from=.git/info/exclude *.bat
-git config core.autocrlf true
